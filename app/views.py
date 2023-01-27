7c5d5ba51303f3973c87991d1741d628248912f3
Ganeshdanthaloori <danthalooriganesh@gmail.com>
2023-01-27 15:05:08 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from app.models import *
+
+# Create your views here.
+
+
+def dept(request):
+    qsd=Department.objects.all()
+    d={'dept':qsd}
+
+    return render(request,'depart.html',d)
+
+
+
+def emp(request):
+    qse=Employee.objects.all()
+    d={'emp':qse}
+
+    return render(request,'employee.html',d)
+
+
+
